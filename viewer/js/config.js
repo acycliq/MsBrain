17f7011f603d99725f419aba368b3b6f5faf8f95
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-06-21 23:21:19 +0100

    first commit

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -1,11 +1,11 @@
 function config() {
     return {
-            roi: {"x0": -5071.627079999999, "x1": 4622.530679999998, "y0": -6427.59084, "y1": 221.184 },
-            imageSize: [262144, 179792],
-            tiles: 'https://www.acycliq.xyz/MsBrain_Eg1_VS6_JH_V6/region_0/262144px/z3/{z}/{y}/{x}.jpg',
-            cellData: 'https://api.github.com/repos/acycliq/VS6_MsBrain_A3_datastore/contents/data/z3/cellData?ref=main',
-            geneData: 'https://api.github.com/repos/acycliq/VS6_MsBrain_A3_datastore/contents/data/z3/geneData?ref=main',
-            cellBoundaries: 'https://api.github.com/repos/acycliq/VS6_MsBrain_A3_datastore/contents/data/z3/cellBoundaries?ref=main',
+            roi: {"x0": 0, "x1": 89917, "y0": 0, "y1": 64016 },
+            imageSize: [262144, 262144 * 64016 / 89917],
+            tiles: 'https://www.acycliq.xyz/MsBrain_Eg1_VS6_JH_V6/region_0/262144px/{z}/{y}/{x}.jpg',
+            cellData: 'https://api.github.com/repos/acycliq/MsBrain/contents/viewer/data/MsBrain_Eg1_VS6_JH_V6/region_0/cellData?ref=master',
+            geneData: 'https://api.github.com/repos/acycliq/MsBrain/contents/viewer/data/MsBrain_Eg1_VS6_JH_V6/region_0/geneData?ref=master',
+            cellBoundaries: 'https://api.github.com/repos/acycliq/MsBrain/contents/viewer/data/MsBrain_Eg1_VS6_JH_V6/region_0/cellBoundaries?ref=master',
             class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
         }
 }
