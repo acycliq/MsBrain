6dc46aabcef4dc9adba43fe5be8a111cb5235c89
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-07-03 11:57:29 +0100

    updated with EG4, EG5

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -70,21 +70,20 @@ function config() {
             cellBoundaries: 'https://api.github.com/repos/acycliq/MsBrain_EG4_VS6library_V6_LH/contents/region_0/cellBoundaries?ref=master',
             class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
         },
-        // {   // 8.
-        //     // MISSING MANIFEST!!
-        //     name: 'MsBrain_Eg3_VS6_JH_V6_region_1',
-        //     roi: {"x0": 0, "x1": 68721, "y0": 0, "y1": 40941},
-        //     imageSize: [262144, 262144 * 40941 / 68721],
-        //     tiles: 'https://www.acycliq.xyz/MsBrain_Eg1_VS6_JH_V6/region_0/262144px/{z}/{y}/{x}.jpg',
-        //     cellData: 'https://api.github.com/repos/acycliq/MsBrain/contents/viewer/data/MsBrain_Eg1_VS6_JH_V6/region_0/cellData?ref=master',
-        //     geneData: 'https://api.github.com/repos/acycliq/MsBrain/contents/viewer/data/MsBrain_Eg1_VS6_JH_V6/region_0/geneData?ref=master',
-        //     cellBoundaries: 'https://api.github.com/repos/acycliq/MsBrain/contents/viewer/data/MsBrain_Eg1_VS6_JH_V6/region_0/cellBoundaries?ref=master',
-        //     class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
-        // },
+        {   // 8.
+            name: 'MsBrain_EG4_VS6library_V6_LH_region_1',
+            roi: {"x0": 0, "x1": 84388, "y0": 0, "y1": 60002},
+            imageSize: [262144, 262144 * 60002 / 84388, ],
+            tiles: 'https://www.acycliq.xyz/MsBrain_EG4_VS6library_V6_LH/region_1/262144px/{z}/{y}/{x}.jpg_ZZZ',
+            cellData: 'https://api.github.com/repos/acycliq/MsBrain_EG4_VS6library_V6_LH/contents/region_1/cellData?ref=master',
+            geneData: 'https://api.github.com/repos/acycliq/MsBrain_EG4_VS6library_V6_LH/contents/region_1/geneData?ref=master',
+            cellBoundaries: 'https://api.github.com/repos/acycliq/MsBrain_EG4_VS6library_V6_LH/contents/region_1/cellBoundaries?ref=master',
+            class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
+        },
         {   // 9.
             name: 'MsBrain_Eg5_VS6_JH_V6_region_0',
-            roi: {"x0": 0, "x1": 80570, "y0": 0, "y1": 62062},
-            imageSize: [262144, 262144 * 62062 / 80570, ],
+            roi: {"x0": 0, "x1": 77981, "y0": 0, "y1": 59461},
+            imageSize: [262144, 262144 * 59461 / 77981, ],
             tiles: 'https://www.acycliq.xyz/MsBrain_Eg5_VS6_JH_V6_05-16-2021/region_0/262144px/{z}/{y}/{x}.jpg_ZZZ',
             cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg5_VS6_JH_V6/contents/region_0/cellData?ref=master',
             geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg5_VS6_JH_V6/contents/region_0/geneData?ref=master',
