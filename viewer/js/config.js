af568722ad65c9e123bf2db30c3570420f33980a
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-07-04 20:20:00 +0100

    correct width and height dimensions for the rotated images

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -2,9 +2,9 @@ function config() {
     var ini = [
         {   // 1.
             name: 'default',
-            roi: {"x0": 0, "x1": 89917, "y0": 0, "y1": 64016},
-            imageSize: [262144, 262144 * 64016 / 89917],
-            tiles: 'https://www.acycliq.xyz/MsBrain_Eg1_VS6_JH_V6/region_0/262144px/{z}/{y}/{x}.jpg_ZZZ',
+            roi: {"x0": 0, "x1": 98824, "y0": 0, "y1": 77294},
+            imageSize: [262144, 262144 * 77294 / 98824],
+            tiles: 'https://raw.githubusercontent.com/acycliq/MsBrain_Eg1_VS6_JH_V6/master/region_0/map_tiles/262144px/{z}/{y}/{x}.jpg',
             cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_0/cellData?ref=master',
             geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_0/geneData?ref=master',
             cellBoundaries: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_0/cellBoundaries?ref=master',
@@ -12,8 +12,8 @@ function config() {
         },
         {   // 2.
             name: 'MsBrain_Eg1_VS6_JH_V6_region_1',
-            roi: {"x0": 0, "x1": 89523, "y0": 0, "y1": 58294},
-            imageSize: [262144, 262144 * 58294 / 89523],
+            roi: {"x0": 0, "x1": 99001, "y0": 0, "y1": 74305},
+            imageSize: [262144, 262144 * 74305 / 99001],
             tiles: 'https://www.acycliq.xyz/MsBrain_Eg1_VS6_JH_V6/region_1/262144px/{z}/{y}/{x}.jpg_ZZZ',
             cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_1/cellData?ref=master',
             geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_1/geneData?ref=master',
@@ -22,8 +22,8 @@ function config() {
         },
         {   // 3.
             name: 'MsBrain_Eg2_VS6_V11_JH_region_0',
-            roi: {"x0": 0, "x1": 77981, "y0": 0, "y1": 70573},
-            imageSize: [262144, 262144 * 70573 / 77981],
+            roi: {"x0": 0, "x1": 105016, "y0": 0, "y1": 103921},
+            imageSize: [262144, 262144 * 103921 / 105016],
             tiles: 'https://www.acycliq.xyz/MsBrain_Eg2_VS6_V11_JH_05-02-2021/region_0/262144px/{z}/{y}/{x}.jpg_ZZZ',
             cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg2_VS6_V11_JH/contents/region_0/cellData?ref=master',
             geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg2_VS6_V11_JH/contents/region_0/geneData?ref=master',
@@ -32,8 +32,8 @@ function config() {
         },
         {   // 4.
             name: 'MsBrain_Eg2_VS6_V11_JH_region_1',
-            roi: {"x0": 0, "x1": 68721, "y0": 0, "y1": 40941},
-            imageSize: [262144, 262144 * 40941 / 68721],
+            roi: {"x0": 0, "x1": 78465, "y0": 0, "y1": 76408},
+            imageSize: [262144, 262144 * 76408 / 78465],
             tiles: 'https://www.acycliq.xyz/MsBrain_Eg2_VS6_V11_JH_05-02-2021/region_1/262144px/{z}/{y}/{x}.jpg_ZZZ',
             cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg2_VS6_V11_JH/contents/region_1/cellData?ref=master',
             geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg2_VS6_V11_JH/contents/region_1/geneData?ref=master',
@@ -42,8 +42,8 @@ function config() {
         },
         {   // 5.
             name: 'MsBrain_Eg3_VS6_JH_V6_region_0',
-            roi: {"x0": 0, "x1": 59766, "y0": 0, "y1": 84513},
-            imageSize: [262144 * 59766 / 84513, 262144],
+            roi: {"x0": 0, "x1": 97102, "y0": 0, "y1": 79603},
+            imageSize: [262144, 262144 * 79603 / 97102],
             tiles: 'https://www.acycliq.xyz/MsBrain_Eg3_VS6_JH_V6/region_0/262144px/{z}/{y}/{x}.jpg_ZZZ',
             cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg3_VS6_JH_V6/contents/region_0/cellData?ref=master',
             geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg3_VS6_JH_V6/contents/region_0/geneData?ref=master',
@@ -52,8 +52,8 @@ function config() {
         },
         {   // 6.
             name: 'MsBrain_Eg3_VS6_JH_V6_region_1',
-            roi: {"x0": 0, "x1": 58115, "y0": 0, "y1": 84430},
-            imageSize: [262144 * 58115 / 84430, 262144],
+            roi: {"x0": 0, "x1": 93968, "y0": 0, "y1": 73157},
+            imageSize: [262144, 262144 * 73157 / 93968, ],
             tiles: 'https://www.acycliq.xyz/MsBrain_Eg3_VS6_JH_V6/region_1/262144px/{z}/{y}/{x}.jpg_ZZZ',
             cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg3_VS6_JH_V6/contents/region_1/cellData?ref=master',
             geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg3_VS6_JH_V6/contents/region_1/geneData?ref=master',
