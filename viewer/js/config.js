31519c048db14ca036b9ada134b8c6363707cc5e
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-07-20 00:39:11 +0100

    first go with celltyped data

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -5,9 +5,9 @@ function config() {
             roi: {"x0": 0, "x1": 98824, "y0": 0, "y1": 77294},
             imageSize: [262144, 262144 * 77294 / 98824],
             tiles: 'https://raw.githubusercontent.com/acycliq/MsBrain_Eg1_VS6_JH_V6/master/region_0/map_tiles/262144px/{z}/{y}/{x}.jpg',
-            cellData: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_0/cellData?ref=master',
-            geneData: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_0/geneData?ref=master',
-            cellBoundaries: 'https://api.github.com/repos/acycliq/MsBrain_Eg1_VS6_JH_V6/contents/region_0/cellBoundaries?ref=master',
+            cellData: 'https://api.github.com/repos/acycliq/vizgen_celltyped/contents/region_0/cellData?ref=master',
+            geneData: 'https://api.github.com/repos/acycliq/vizgen_celltyped/contents/region_0/geneData?ref=master',
+            cellBoundaries: 'https://api.github.com/repos/acycliq/vizgen_celltyped/contents/region_0/cellBoundaries?ref=master',
             class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
         },
         {   // 2.
