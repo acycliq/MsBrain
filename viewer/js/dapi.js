aa796538c27e2abee901fdb736f3e2a6c5ed59bd
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-07-20 10:00:21 +0100

    wiring up the glyph color

--- viewer/js/dapi.js
+++ viewer/js/dapi.js
@@ -159,7 +159,7 @@ function dapi(cfg) {
                 // "Cell_Num": origin.Cell_Num,
                 "fromPoint": fromPoint,
                 "toPoint": toPoint,
-                "color": getColor(getTaxonomy(gene)),
+                "color": getColor(gene),
                 // "color": getColor(glyphMap.get(gene).taxonomy),
             };
 
